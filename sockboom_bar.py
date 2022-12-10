004116a3d2cbef881ddd9c45093a58e1886f06c3
Jingjie-cloud <62613126+Jingjie-cloud@users.noreply.github.com>
2022-12-11 01:52:04 +0800

    Update sockboom_bar.py

--- sockboom_bar.py
+++ sockboom_bar.py
@@ -25,7 +25,7 @@ def sockboom():
     sure = driver.find_element_by_xpath("/html/body/div[3]/div[7]/div/button")#看看有没有 ‘DNS投毒’的‘确认’
     ActionChains(driver).move_to_element(sure).click().perform()
     
-    chick_in = driver.find_element_by_css_selector("div.card-action>div>p>button")
+    chick_in = driver.find_element_by_css_selector("div.card>card-main>card-action>card-action-btn pull-left>p>button")
     ActionChains(driver).click(chick_in).perform()
     
     
