47b7a50933df28e5af1a777efe6dc969fa275791
Jingjie-cloud <62613126+Jingjie-cloud@users.noreply.github.com>
2022-12-11 23:40:08 +0800

    Update sockboom_bar.py

--- sockboom_bar.py
+++ sockboom_bar.py
@@ -20,15 +20,16 @@ def sockboom():
     #driver.find_element_by_xpath("//*[@style='display: inline-block; background-color: rgb(140, 212, 245); box-shadow: rgba(140, 212, 245, 0.8) 0px 0px 2px, rgba(0, 0, 0, 0.05) 0px 0px 0px 1px inset;']").click()
     driver.find_element_by_xpath("/html/body/div[3]/div[7]/div/button").click()
     driver.implicitly_wait(3)
+    time.sleep(3)
     #chick_in = driver.find_element_by_xpath("/html/body/div[1]/div[2]/main/section/div/section[2]/div/div[2]/div[2]/div/div[2]/div/p/button")
     
     sure = driver.find_element_by_xpath("/html/body/div[3]/div[7]/div/button")#看看有没有 ‘DNS投毒’的‘确认’
     ActionChains(driver).move_to_element(sure).click().perform()
     driver.implicitly_wait(3)
     chick_in = driver.find_element_by_xpath("//*[@class='i-button button-check']")
-    driver.execute_script("arguments[0].scrollIntoView();", chick_in)
-    driver.implicitly_wait(3)
-    chick_in.click()
+#     driver.execute_script("arguments[0].scrollIntoView();", chick_in)
+#     driver.implicitly_wait(3)
+#     chick_in.click()
     
     
     
