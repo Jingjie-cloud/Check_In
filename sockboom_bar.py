64883e638613faf9b5f2b250300e9261e758d216
Jingjie-cloud <62613126+Jingjie-cloud@users.noreply.github.com>
2022-12-12 19:45:37 +0800

    Update sockboom_bar.py

--- sockboom_bar.py
+++ sockboom_bar.py
@@ -21,12 +21,14 @@ def sockboom():
     driver.find_element_by_xpath("/html/body/div[3]/div[7]/div/button").click()
     driver.implicitly_wait(10)
     time.sleep(5)
+    print(driver.find_element_by_id("catcat").text)
     #chick_in = driver.find_element_by_xpath("/html/body/div[1]/div[2]/main/section/div/section[2]/div/div[2]/div[2]/div/div[2]/div/p/button")
     
 #     sure = driver.find_element_by_xpath("/html/body/div[3]/div[7]/div/button")#看看有没有 ‘DNS投毒’的‘确认’
 #     ActionChains(driver).move_to_element(sure).click().perform()
     driver.find_element_by_xpath("/html/body/div[1]/div[2]/main/section/div/section[2]/div/div[1]/div[2]/div/div/div/div[1]/div/div/div[8]/div/span[4]").click()
     driver.implicitly_wait(3)
+    
     
 #     print(driver.find_element_by_xpath("//*[@class='i-button button-check']").is_enabled())
     
